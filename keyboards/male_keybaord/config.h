c2d010f550d83ff868c4d60f7deb812310687bae
Max <max.joel.mj@googlemail.com>
2026-04-09 21:23:34 +0200

    place left and right keys into folders

--- keyboards/male_keybaord/config.h
+++ keyboards/male_keybaord/config.h
@@ -3,20 +3,6 @@
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 5
 
-#define DIRECT_PINS { \
-    { GP0, GP1, GP2, GP3, GP4 }, \
-    { GP5, GP6, GP7, GP8,  GP9 }, \
-    { GP12, GP13, GP14, GP15, GP16 }, \
-    { GP21, GP23, GP20, NO_PIN, NO_PIN } \
-}
-
-#define DIRECT_PINS_RIGHT {  \
-    { GP0, GP1, GP2, GP3, GP29 }, \
-    { GP28, GP27, GP26, GP22, GP20 }, \
-    { GP23, GP21, GP16, GP15, GP14 }, \
-    { GP9, GP12, GP13, NO_PIN, NO_PIN } \
-}
-
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
--- keyboards/male_keybaord/left/config.h
+++ keyboards/male_keybaord/left/config.h
@@ -1,4 +1,19 @@
 #pragma once
 
 #include "../config.h"
-#define SERIAL_USART_TX_PIN GP22
+
+/*LEFT*/  
+#define DIRECT_PINS { \
+    { GP0, GP1, GP2, GP3, GP4 }, \
+    { GP5, GP6, GP7, GP8,  GP9 }, \
+    { GP12, GP13, GP14, GP15, GP16 }, \
+    { GP21, GP23, GP20, NO_PIN, NO_PIN } \
+}
+
+/*RIGHT*/
+/*#define DIRECT_PINS {  \
+    { GP0, GP1, GP2, GP3, GP29 }, \
+    { GP28, GP27, GP26, GP22, GP20 }, \
+    { GP23, GP21, GP16, GP15, GP14 }, \
+    { GP9, GP12, GP13, NO_PIN, NO_PIN } \
+}*/
