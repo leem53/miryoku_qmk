f04d98a03f90d61b30e1c31a17329a1fc82ae318
Max <max.joel.mj@googlemail.com>
2026-04-09 21:28:04 +0200

    add serial_usart_tx_pin again

--- keyboards/male_keybaord/left/config.h
+++ keyboards/male_keybaord/left/config.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../config.h"
+#define SERIAL_USART_TX_PIN GP22
 
 /*LEFT*/  
 #define DIRECT_PINS { \
--- keyboards/male_keybaord/right/config.h
+++ keyboards/male_keybaord/right/config.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../config.h"
+#define SERIAL_USART_TX_PIN GP8
 
 /*LEFT*/  
 /*#define DIRECT_PINS { \
